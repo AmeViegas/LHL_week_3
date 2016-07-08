5ecd04def6f2447bd29d108a530c152b4f38d627
ame viegas <ameeta.viegas@gmail.com>
2016-07-07 22:20:16 -0400

    assignment students/teachers

--- d3/ar-students-teachers-master/db/migrate/20160707175555_create_teachers.rb
+++ d3/ar-students-teachers-master/db/migrate/20160707175555_create_teachers.rb
@@ -1,18 +1,15 @@
-class CreateStudents < ActiveRecord::Migration
+class CreateTeachers < ActiveRecord::Migration
 
   def change
     # Add code to create the table here
     # HINT: check out ActiveRecord::Migration.create_table
-    create_table :students do |t|
+    create_table :teachers do |t|
       # column definitions go here
       # Use the AR migration guide for syntax reference
-      t.string :first_name
-      t.string :last_name
-      t.string :gender
+      t.string :name
       t.string :email
+      t.text :address
       t.string :phone
-      t.date   :birthday
-      # t.timestamps null: false
       t.datetime :created_at
       t.datetime :updated_at
     end
---
+++ d3/ar-students-teachers-master/spec/teacher_data_spec.rb
@@ -0,0 +1,11 @@
+require_relative 'spec_helper'
+
+describe Teacher, 'Sample data' do
+  before :each do
+    Teacher.create(name: "Jim Beam", email:"jbeam@gmail.com", address: "5 list st. city,st 39296", phone: "393-393-2299")
+  end
+
+  it 'should create a teacher' do
+    expect(Teacher.where('name = ?', 'Jim Beam').exists?).to be(true)
+  end
+end
